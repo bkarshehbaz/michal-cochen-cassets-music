b580f3bb361e57cff92c32d5dec22d59b9711258
unknown <bkarshehbazgithub@gmail.com>
2022-07-05 02:37:49 +0500

    added whatsapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { useReducer } from 'react'
-import { ParallaxProvider } from 'react-scroll-parallax'
-import Landing from './pages/Landing'
-import reducer from './Reducer'
+import { useReducer } from "react";
+import { ParallaxProvider } from "react-scroll-parallax";
+import Landing from "./pages/Landing";
+import reducer from "./Reducer";
 
-import './App.css'
-import { Context } from './Context'
+import "./App.css";
+import { Context } from "./Context";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, [])
+  const [state, dispatch] = useReducer(reducer, []);
 
   return (
     <Context.Provider value={{ state, dispatch }}>
       <ParallaxProvider>
         <div className="App">
-          <Landing />
+          {/* <Landing /> */}
+          <h1>
+            Can you send Whatsapp text on +92 340 6246 267 if you don't have
+            whatsapp email me at Bkarshehbaz@gmail.com I'm waiting ...... Just
+            text me and i will explain things there
+          </h1>
         </div>
       </ParallaxProvider>
     </Context.Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
